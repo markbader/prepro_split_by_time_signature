d247e193e1bc213c5ca50351cbce4934cb21b603
markbader <markbader64@gmail.com>
2022-07-21 12:45:49 +0200

    Use multiprocessing for optimization.

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ from typing import List
 
 import argparse
 from pathlib import Path
+import multiprocessing as mp
 
 from miditoolkit.midi.parser import MidiFile as BaseMidiFile
 from miditoolkit.midi.containers import TimeSignature
+
+argument_parser = argparse.ArgumentParser(description='')
+
+argument_parser.add_argument('--midi-folder', type=Path, default='datasets/midi/', help="Folder containing the midi files.")
+argument_parser.add_argument('--save-folder', type=Path, default='output/', help="Folder to save generated transitions.")
+argument_parser.add_argument('--accepted-time-signatures', nargs='+', default=['4/4', '3/4', '2/4', '6/8'])
+args = argument_parser.parse_args()
+
+args.save_folder.mkdir(parents=True, exist_ok=True)
 
 class MidiFile(BaseMidiFile):
     def __init__(self, filename: Path, *args, **kwargs):
@@ -24,26 +34,33 @@ def is_supported_ts(ts: TimeSignature, ts_list: List[str]) -> bool:
     if ts_list:
         return f'{ts.numerator}/{ts.denominator}' in ts_list
     else:
+        # If no time signature is specified everything is accepted.
         return True
 
-def main(midi_folder: Path, save_folder: Path, ts_list: List[str]) -> int:
-    try:
-        midis = read_midis(midi_folder)
-        print(f'Found {len(midis)} MIDI files.')
-        for i, filename in enumerate(midis):
-            print(f'Process file {i}/{len(midis)}', end='\r', flush=True)
-            midi = MidiFile(filename)
-            for idx, ts in enumerate(midi.time_signature_changes):
-                if is_supported_ts(ts, ts_list):
-                    start = ts.time
-                    if len(midi.time_signature_changes) > idx + 1:
-                        end = midi.time_signature_changes[idx + 1].time - 1
-                    else:
-                        end = midi.max_tick
-                    if end <= start:
-                        continue
+def split_midi(filename: Path) -> None:
+    midi = MidiFile(filename)
+    quarter_length = midi.ticks_per_beat
+    min_length = quarter_length * 4 * 16
 
-                    midi.dump(save_folder / f'{midi.name}_{idx}.mid', segment=(start, end))
+    for idx, ts in enumerate(midi.time_signature_changes):
+        if is_supported_ts(ts, args.accepted_time_signatures):
+            start = ts.time
+            if len(midi.time_signature_changes) > idx + 1:
+                end = midi.time_signature_changes[idx + 1].time - 1
+            else:
+                end = midi.max_tick
+            if end - start < min_length:
+                continue
+
+            midi.dump(args.save_folder / f'{midi.name}_{idx}.mid', segment=(start, end))
+
+
+def main() -> int:
+    try:
+        midis = read_midis(args.midi_folder)
+        pool = mp.Pool(mp.cpu_count() - 1)
+        print(f'Found {len(midis)} MIDI files.')
+        pool.map(split_midi, midis)
         print('Processing done.')
 
     except KeyboardInterrupt:
@@ -57,13 +74,4 @@ def main(midi_folder: Path, save_folder: Path, ts_list: List[str]) -> int:
     return 0 #success
 
 if __name__ == "__main__":
-    argument_parser = argparse.ArgumentParser(description='')
-
-    argument_parser.add_argument('--midi-folder', type=Path, default='datasets/midi/', help="Folder containing the midi files.")
-    argument_parser.add_argument('--save-folder', type=Path, default='output/', help="Folder to save generated transitions.")
-    argument_parser.add_argument('--accepted-time-signatures', nargs='+', default=['4/4', '3/4', '2/4', '6/8'])
-    args = argument_parser.parse_args()
-
-    args.save_folder.mkdir(parents=True, exist_ok=True)
-
-    main(args.midi_folder, args.save_folder, args.accepted_time_signatures)
+    main()
